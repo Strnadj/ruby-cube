d69c0d5b6baf03f4a8fb58a931c2bbd0e9b21396
Strnadj <jan.strnadek@gmail.com>
2017-07-16 22:07:34 +0200

    Test

--- ext/ruby_cube/ruby_cube.c
+++ ext/ruby_cube/ruby_cube.c
@@ -1,12 +1,47 @@
-#include "ruby.h"
+#include <ruby.h>
 
-static VALUE hello_world(VALUE klass)
-{
-  return rb_str_new2("hello world");
+/**
+ * Initialize method.
+ */
+static VALUE t_init(VALUE self, VALUE hash) {
+  Check_Type(hash, T_HASH);
+
+  // Set some basic variables
+  rb_iv_set(self, "@axes", hash);
+  rb_iv_set(self, "@dimension", RHASH_SIZE(hash));
+
+  // Sizes are just values from input hash.values
+  VALUE sizes = rb_funcall(hash, rb_intern("values"), 0);
+  VALUE keys  = rb_funcall(sizes, rb_intern("flatten"), 0);
+  rb_iv_set(self, "@sizes", sizes);
+
+  // Test for duplicate keys
+  rb_iv_set(self, "@keys", keys);
+  rb_iv_set(self, "@test", RARRAY_LEN(rb_iv_get(self, "@keys")));
+  // int normalSize = FIX2INT(RARRAY_LEN(sizes));
+  // int uniqueSize = FIX2INT(RARRAY_LEN(rb_funcall(sizes, rb_intern("uniq"), 0)));
+
+  //rb_iv_set(self, "@normal", sizes);
+  //rb_iv_set(self, "@uniq", rb_funcall(sizes, rb_intern("uniq"), 0));
+
+ //if (normalSize != uniqueSize) {
+ //  rb_raise(rb_eTypeError, "Cannot use one key twice!");
+ //  return self;
+ //}
+
+
+
+  return self;
 }
+
 
 void Init_ruby_cube()
 {
   VALUE mRubyCube = rb_define_class("RubyCube", rb_cObject);
-  rb_define_singleton_method(mRubyCube, "hello_world", hello_world, 0);
+  rb_define_method(mRubyCube, "initialize", (VALUE(*)(ANYARGS))t_init, 1);
+
+  // Define attr acessors
+  rb_define_attr(mRubyCube, "data", 1, 1);
+  rb_define_attr(mRubyCube, "sizes", 1, 0);
+  rb_define_attr(mRubyCube, "dimension", 1, 0);
 }
--- test/test_ruby_cube.rb
+++ test/test_ruby_cube.rb
@@ -2,7 +2,39 @@ require "minitest/autorun"
 require "ruby_cube"
 
 class TestRubyCube < Minitest::Test
-  def test_sanity
-    assert_equal "hello world", RubyCube.hello_world
+  def test_axes
+    @test_axes ||= {
+      x: %i(a b c),
+      y: %i(d e f)
+    }
+  end
+
+  def test_cube
+    @cube ||= RubyCube.new(test_axes)
+  end
+
+  def test_invalid_initialize
+    assert_raises TypeError do
+      RubyCube.new(%i(arg1 arg2))
+    end
+  end
+
+  def test_duplicity_key
+    assert_raises TypeError do
+      a = RubyCube.new({
+        a: %i(a b),
+        b: %i(b c)
+      })
+
+      puts a.instance_variable_get(:@sizes).inspect
+      puts a.instance_variable_get(:@keys).inspect
+      puts a.instance_variable_get(:@test).inspect
+    end
+  end
+
+  def test_valid_initialize
+    assert_equal test_cube.instance_variable_get(:@axes), test_axes
+    assert_equal test_cube.dimension, 2
+    assert_equal test_cube.sizes, test_axes.values
   end
 end
